0fa127caba94e881b917820e79d905fc123beedb
Andrey Stelmakh <andrey.stelmakh@live.ru>
2025-09-24 23:43:17 +0100

    Добавьте файлы проекта.

---
+++ UWPSocionica/Controls/CheckBox_Feature.cs
@@ -0,0 +1,13 @@
+using System.Windows;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace UWPSocionica.Controls
+{
+    internal partial class CheckBox_Feature : CheckBox
+    {
+        static CheckBox_Feature()
+        {
+        }
+    }
+}
